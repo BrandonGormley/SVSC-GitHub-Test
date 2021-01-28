8bc60795776da48b0c39c46cee14783aad814b34
Brandon Gormley <brandontgormley@gmail.com>
2021-01-28 15:57:16 -0500

    Added Functionality to retrieve additional data
    When clicking on each profile, you retrieve additional data about user

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const btnAngular = document.querySelector('.btn-angular');
 const btnVue = document.querySelector('.btn-vue');
 const githubProfiles = document.querySelector('.profile-container');
 const filterInput = document.querySelector('.search-input');
+const profileInfo = document.querySelector('.profile-info');
 
 // Event Listeners
 btnFacebook.addEventListener('click', getFacebookProfiles);
@@ -23,7 +24,6 @@ async function getFacebookProfiles() {
 
     let output = ``;
     profiles.forEach(async profile => {
-      console.log(profile);
       output += `
         <div class="profile-card">
           <h3>${profile.login}</h3>
@@ -35,6 +35,26 @@ async function getFacebookProfiles() {
   } catch (err) {
     console.log(`Looks like we have an error: ${err}`);
   }
+
+  // Fetching addiitonal data from user
+  const profileCards = document.querySelectorAll('.profile-card');
+  profileCards.forEach(profile => {
+    profile.addEventListener('click', e => {
+      let user = e.target.textContent;
+      console.log(user);
+      fetch(`https://api.github.com/users/${user}`)
+        .then(res => res.json())
+        .then(user => {
+          let userData = `
+          <li>Name: ${user.name}</li>
+          <li>Repos: ${user.repos_url}</li>
+          <li>Followers: ${user.followers}</li>
+          <li>Account Started: ${user.created_at}</li>
+        `;
+          profileInfo.innerHTML = userData;
+        });
+    });
+  });
 }
 
 // Fetch Angular Profiles
